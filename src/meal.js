a26ac2d4676b7beac8de6e9f5b8ba489ddec6da9
Kim Ward <kmewrd@outlook.com>
2021-12-01 20:39:15 -0600

    Complete meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  };
+}
+
+function addIngredients(ingredient, list) {
+  if (list.includes(ingredient)) {
+
+  } else{
+    return list.push(ingredient);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return ((price/10) * 9).toFixed(2);
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
