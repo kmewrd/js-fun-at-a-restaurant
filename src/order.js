a91dc36e7d74d56827e6e180c98256c024d7e026
Kim Ward <kmewrd@outlook.com>
2021-12-02 20:32:20 -0600

    Complete order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,44 @@
+function takeOrder(order, orderTypeList) {
+  if (orderTypeList.length === 3) {
+
+  } else {
+    return orderTypeList.push(order);
+  }
+}
+
+function refundOrder(orderNumber, orderTypeList) {
+  for (var i = 0; i < orderTypeList.length; i++) {
+    if (orderTypeList[i].orderNumber === orderNumber) {
+      orderTypeList.splice(i, 1);
+    } else {
+
+    }
+  }
+}
+
+function listItems(orderTypeList) {
+  var newList = [];
+  for (var i = 0; i < orderTypeList.length; i++) {
+    newList.push(orderTypeList[i].item);
+  }
+  return newList.join(", ");
+}
+
+function searchOrder(orderTypeList, item) {
+  var allItems = [];
+  for (var i = 0; i < orderTypeList.length; i++) {
+    allItems.push(orderTypeList[i].item);
+  }
+  if (allItems.includes(item)) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
