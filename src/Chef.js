beb18359abc30eb67491cab7740da5831d1487b4
Kim Ward <kmewrd@outlook.com>
2021-12-04 13:45:52 -0600

    Complete Chef test

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,20 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+    this.greetCustomer = function(name, isMorning) {
+      if (isMorning === true) {
+        return `Good morning, ${name}!`;
+      } else if (isMorning === undefined || isMorning === false) {
+        return `Hello, ${name}!`;
+      }
+    }
+    this.checkForFood = function(foodItem) {
+      if (restaurant.menus[foodItem.type].includes(foodItem)) {
+        return `Yes, we're serving ${foodItem.name} today!`;
+      }
+      return `Sorry, we aren't serving ${foodItem.name} today.`;
+    }
   }
 }
 
